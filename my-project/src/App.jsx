c606871038dd483996c9686601cbfa6509ea8e7e
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-10-22 23:55:39 +0530

    backend created

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import { useState } from 'react'
 function App() {
-  
-
+  const [name, setName] = useState('')
+  const [datetime, setDatetime] = useState('')
+  const[description,setDescription]  = useState('')
   return (
     <>
       <main>
@@ -9,12 +11,12 @@ function App() {
         <form>
           <div className="basic">
 
-          <input type="text" placeholder="iphone 13 pro" />
-          <input type="datetime-local"/>
+            <input type="text" placeholder="iphone 13 pro" value={name} onChange={ev=>setName(ev.target.value)}/>
+          <input type="datetime-local" value={datetime} onChange={ev=>setDatetime(ev.target.value)}/>
           </div>
           <div className="description">
 
-          <input type="text" placeholder="description"/>
+          <input type="text" placeholder="description" value={description} onChange={ev=>setDescription(ev.target.value)}/>
           </div>
           <button type="submit">Add new transaction</button>
         </form>
@@ -25,7 +27,27 @@ function App() {
               <div className="description">it was time for new washing machine</div>
             </div>
             <div className="right">
-              <div className="price">$800</div>
+              <div className="price red">-$800</div>
+              <div className="datetime">2024-10-12 17:30</div>
+            </div>
+          </div>
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New washing machine</div>
+              <div className="description">it was time for new washing machine</div>
+            </div>
+            <div className="right">
+              <div className="price red">-$800</div>
+              <div className="datetime">2024-10-12 17:30</div>
+            </div>
+          </div>
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New washing machine</div>
+              <div className="description">it was time for new washing machine</div>
+            </div>
+            <div className="right">
+              <div className="price green">+$800</div>
               <div className="datetime">2024-10-12 17:30</div>
             </div>
           </div>
